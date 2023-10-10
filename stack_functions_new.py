ebe76b799fdfac6f8ed1ec53d163b9e690c08862
Lucie Hugonnet <107409392+luciehgnt@users.noreply.github.com>
2023-10-10 17:05:12 +0200

    Add files via upload

--- stack_functions_new.py
+++ stack_functions_new.py
@@ -53,7 +53,7 @@ def make_date(traces):
 
 	return fdate
 
-def make_dir(traces, pcc, parent_dir, folder, stats, start,i, time, window):
+def make_dir(traces, pcc, parent_dir, folder, stats, start_channel,i, time, window):
 	dt = traces['times'][1].item() - traces['times'][0].item()
 	dt = float(dt) * 1e-9
 	try:
@@ -65,11 +65,11 @@ def make_dir(traces, pcc, parent_dir, folder, stats, start,i, time, window):
 	stats.npts = len(pcc)
 	correlogram = Trace(data=pcc, header=stats)
 	fdate = make_date(traces)
-	fname = f'correlogram_{start}_{i}_{int(time*dt)}_{window}_{fdate}'
+	fname = f'{start_channel}_{i}_{int(time*dt)}_{window}_{fdate}'
 	correlogram.write(parent_dir + folder + fname + '.sac', format='sac')		
 
 # define function for cross correlation: one-bit cc, normal cc, and phase cc
-def cross_correlate(cc,traces,parent_dir,window,lag,length,time,channel,start,start_position,
+def cross_correlate(traces,parent_dir,window,lag,length,time,nb_channel,start_channel,start_position,
 			onebit,phase_cc,decimate=1,resample=1):
 	dt = traces['times'][1].item() - traces['times'][0].item()
 	dt = float(dt) * 1e-9
@@ -77,28 +77,35 @@ def cross_correlate(cc,traces,parent_dir,window,lag,length,time,channel,start,st
 
 	t = np.arange(-window, window, dt)
 
+	# make output array
+	if start_position == 'mid':
+		cc = np.zeros((2*int(window/dt),2*nb_channel+1))
+	elif start_position == 'mid_all':
+		cc = np.zeros((2*int(window/dt),430+1))
+		# cc = np.zeros((2*int(window/dt),3249+1))
+	else:
+		cc = np.zeros((2*int(window/dt),nb_channel+1))
+
+	# cross correlate all selected channel in time window
+	if start_position == 'mid':
+		ranges = np.arange(start_channel-nb_channel,start_channel+nb_channel+1,1)
+	elif start_position == 'early':
+		ranges = np.arange(start_channel, start_channel+nb_channel+1, 1)
+	elif start_position == 'end':
+		ranges = np.arange(start_channel, start_channel-nb_channel-1, -1)
+	elif start_position == 'mid_all':
+		ranges = np.arange(56, 430+1, 1)
+		#ranges = np.arange(56, 3250, 1)
+
 	# looping cross correlation over varying time window
 	iteration = 1
-	#pcc_all = np.zeros([channel,cc.shape[0],cc.shape[1]])
+	#pcc_all = np.zeros([nb_channel,cc.shape[0],cc.shape[1]])
 	stats = Stats()
 	stats.delta = dt
 	stats.sampling_rate = 1/stats.delta
 	stats.network = 'Correlogram'
 	stats.station = 'STRMBL'
 	stats.location = 'Italy'
-
-
-	# cross correlate all selected channel in time window
-	if start_position == 'mid':
-		ranges = np.arange(start-channel,start+channel+1,1)
-	elif start_position == 'early':
-		ranges = np.arange(start, start+channel+1, 1)
-	elif start_position == 'end':
-		ranges = np.arange(start, start-channel-1, -1)
-	elif start_position == 'mid_all':
-		ranges = np.arange(56, 430+1, 1)
-		#ranges = np.arange(56, 3250, 1)
-
 
 	while time < length:
 		if iteration%decimate == 0:
@@ -110,25 +117,20 @@ def cross_correlate(cc,traces,parent_dir,window,lag,length,time,channel,start,st
 			#for i in range(57,60): # use this when only interested in some channels
 			
 				if onebit == True:
-					x1 = one_bit(traces[time:time+int(window/dt),start])
+					x1 = one_bit(traces[time:time+int(window/dt),start_channel])
 					x2 = one_bit(traces[time:time+int(window/dt),i_channel])
 				else:
-					x1 = traces[time:time+int(window/dt),start]
+					x1 = traces[time:time+int(window/dt),start_channel]
 					x2 = traces[time:time+int(window/dt),i_channel]
 				
 				if phase_cc == True:
-					# _t , pcc = pcc2(x1, x2, dt, -window, window)
-					# cc[:,j] = cc[:,j] + pcc
-						
-					# folder = f'Correlogram_{start}_{i_channel}_phase/'
-					# make_dir(traces, pcc, parent_dir, folder, stats, start, i_channel, time, window)
 
 					try:
 						_t , pcc = pcc2(x1, x2, dt, -window, window)
 						cc[:,j] = cc[:,j] + pcc
 						
-						folder = f'Correlogram_{start}_{i_channel}_phase/'
-						make_dir(traces, pcc, parent_dir, folder, stats, start, i_channel, time, window)
+						folder = f'Correlogram_{start_channel}_{i_channel}_pcc/'
+						make_dir(traces, pcc, parent_dir, folder, stats, start_channel, i_channel, time, window)
 					
 					except:
 						# print(len(corr),len(cc[:,j])) # show length
@@ -140,8 +142,8 @@ def cross_correlate(cc,traces,parent_dir,window,lag,length,time,channel,start,st
 						corr = signal.correlate(x1,x2)
 						corr = np.append(corr,0) # pad with zero to similarize the array length
 						cc[:,j] = cc[:,j] + corr  
-						folder = f'Correlogram_{start}_{i}_normal/'
-						make_dir(traces, corr, parent_dir, folder, stats, start,i, time, window)
+						folder = f'Correlogram_{start_channel}_{i_channel}_normal/'
+						make_dir(traces, corr, parent_dir, folder, stats, start_channel,i_channel, time, window)
 					except:
 						# print(len(corr),len(cc[:,j])) # show length
 						print(f'Time window starting at {time*dt} s is shorter. Finishing the program ...')
@@ -153,7 +155,7 @@ def cross_correlate(cc,traces,parent_dir,window,lag,length,time,channel,start,st
 	return cc, t
 
 # define function for auto correlation: one-bit cc, normal cc, and phase cc
-def auto_correlate(cc,traces,parent_dir,window,lag,length,time,channel,start,start_position,
+def auto_correlate(traces,parent_dir,window,lag,length,time,nb_channel,start_channel,start_position,
 			onebit,phase_cc,decimate=1,resample=1):
 	
 	dt = traces['times'][1].item() - traces['times'][0].item()
@@ -162,26 +164,35 @@ def auto_correlate(cc,traces,parent_dir,window,lag,length,time,channel,start,sta
 
 	t = np.arange(-window, window, dt)
 
+	# make output array
+	if start_position == 'mid':
+		cc = np.zeros((2*int(window/dt),2*nb_channel+1))
+	elif start_position == 'mid_all':
+		cc = np.zeros((2*int(window/dt),430+1))
+		# cc = np.zeros((2*int(window/dt),3249+1))
+	else:
+		cc = np.zeros((2*int(window/dt),nb_channel+1))
+
+	# cross correlate all selected channel in time window
+	if start_position == 'mid':
+		ranges = np.arange(start_channel-nb_channel,start_channel+nb_channel+1,1)
+	elif start_position == 'early':
+		ranges = np.arange(start_channel, start_channel+nb_channel+1, 1)
+	elif start_position == 'end':
+		ranges = np.arange(start_channel, start_channel-nb_channel-1, -1)
+	elif start_position == 'mid_all':
+		ranges = np.arange(56, 430+1, 1)
+		#ranges = np.arange(56, 3250, 1)
+
 	# looping auto correlation over varying time window
 	iteration = 1
-	#pcc_all = np.zeros([channel,cc.shape[0],cc.shape[1]])
+	#pcc_all = np.zeros([nb_channel,cc.shape[0],cc.shape[1]])
 	stats = Stats()
 	stats.delta = dt
 	stats.sampling_rate = 1/stats.delta
 	stats.network = 'Correlogram'
 	stats.station = 'STRMBL'
 	stats.location = 'Italy'
-
-	# cross correlate all selected channel in time window
-	if start_position == 'mid':
-		ranges = np.arange(start-channel,start+channel+1,1)
-	elif start_position == 'early':
-		ranges = np.arange(start, start+channel+1, 1)
-	elif start_position == 'end':
-		ranges = np.arange(start, start-channel-1, -1)
-	elif start_position == 'mid_all':
-		ranges = np.arange(56, 430+1, 1)
-		#ranges = np.arange(56, 3250, 1)
 
 	while time < length:
 		if iteration%decimate == 0:
@@ -200,8 +211,8 @@ def auto_correlate(cc,traces,parent_dir,window,lag,length,time,channel,start,sta
 						_t , pcc = apcc2(x1, dt, -window, window)
 						cc[:,j] = cc[:,j] + pcc # linear stack
 						
-						folder = f'Correlogram_{start}_{i_channel}_phase/'
-						make_dir(traces, pcc, parent_dir, folder, stats, start,i_channel, time, window)
+						folder = f'Correlogram_{start_channel}_{i_channel}_pcc/'
+						make_dir(traces, pcc, parent_dir, folder, stats, start_channel,i_channel, time, window)
 							
 					except:
 						# print(len(corr),len(cc[:,j])) # show length
@@ -213,8 +224,8 @@ def auto_correlate(cc,traces,parent_dir,window,lag,length,time,channel,start,sta
 						corr = signal.correlate(x1,x1)
 						corr = np.append(corr,0) # pad with zero to similarize the array length
 						cc[:,j] = cc[:,j] + corr  
-						folder = f'Correlogram_{start}_{i_channel}_normal/'
-						make_dir(traces, corr, parent_dir, folder, stats, start,i_channel, time, window)
+						folder = f'Correlogram_{start_channel}_{i_channel}_normal/'
+						make_dir(traces, corr, parent_dir, folder, stats, start_channel,i_channel, time, window)
 					except:
 						# print(len(corr),len(cc[:,j])) # show length
 						print(f'Time window starting at {time*dt} s is shorter. Finishing the program ...')
